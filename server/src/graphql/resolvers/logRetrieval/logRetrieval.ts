d3e0f2b6de8b8ea534ad07fbf4e379336203e10b
valgaug <valentin.gaugain@hotmail.fr>
2024-01-16 17:42:33 +0100

    wip

--- server/src/graphql/resolvers/logRetrieval/logRetrieval.ts
+++ server/src/graphql/resolvers/logRetrieval/logRetrieval.ts
@@ -3,8 +3,8 @@ import { accessLog } from '../../../db/accessLog';
 
 const queryResolvers: QueryResolvers = {
   getAccessLogs: (_parent, _args, context) => {
-    if (!context.user.isAdmin) {
-      throw new Error('Unauthorized access');
+    if (!context.user || !context.user.isAdmin) {
+      throw new Error('Unauthorized! You need to be an Admin.');
     }
     return accessLog;
   },
